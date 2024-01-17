97c9cae03414cd987d3cbafcf13f1bc145b3f7ab
Dragon Style Studio <natthapoom.somsuk@gmail.com>
2024-01-17 17:23:54 +0700

    commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+ob_start();
+session_start();
+include('./controllers/con_db.php');
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TATC-Delivery </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.4/r-2.4.1/sb-1.4.2/sp-2.1.2/datatables.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="./public/css/main.css">
+    <link rel="stylesheet" href="./public/css/style.css">
+    
+</head>
+
+<body>
+    <?php
+    if (empty($_GET['page']) || !ctype_alnum(str_replace(['-', '_'], '', $_GET['page'])) || !file_exists("pages/{$_GET['page']}.php")) {
+        die(header('Location: ?page=login'));
+    }
+    require_once "pages/{$_GET['page']}.php";
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/r-2.4.1/sb-1.4.2/sp-2.1.2/datatables.min.js"></script>
+   
+</body>
+
+</html>
---
+++ pages/register_employee.php
@@ -0,0 +1,88 @@
+<div class=" bg-main vh-100 vw-100 p-5">
+    <div class=" card rounded-0 bg-black bg-opacity-10 border-0 h-100">
+        <div class=" card-body p-5">
+            <p class=" display-1 text-center">Regis For Employee</p>
+            <form>
+                <div class=" row row-cols-2 g-5">
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">รูปภาพ</p>
+                            <input type="file" name="" id="" class=" form-control shadow-none ">
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">สถานะ</p>
+                            <select class="form-select">
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">ชื่อ-นามสกุล :</p>
+                            <input type="text" name="name"
+                                class=" fs-4  form-control border-top-0 border-start-0 border-end-0 rounded-0 border-3 border-dark bg-transparent shadow-none"
+                                required>
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">ตรางสอน</p>
+                            <input type="file" name="" id="" class=" form-control shadow-none ">
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">เบอร์โทร :</p>
+                            <input type="text" name="name"
+                                class=" fs-4  form-control border-top-0 border-start-0 border-end-0 rounded-0 border-3 border-dark bg-transparent shadow-none"
+                                required>
+                        </div>
+                    </div>
+
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">QRCod บัญชีธนาคาร</p>
+                            <input type="file" name="" id="" class=" form-control shadow-none ">
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">แผนกวิชา :</p>
+                            <input type="text" name="name"
+                                class=" fs-4  form-control border-top-0 border-start-0 border-end-0 rounded-0 border-3 border-dark bg-transparent shadow-none"
+                                required>
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">username :</p>
+                            <input type="text" name="name"
+                                class=" fs-4  form-control border-top-0 border-start-0 border-end-0 rounded-0 border-3 border-dark bg-transparent shadow-none"
+                                required>
+                        </div>
+                    </div>
+                    <div class=" col">
+                        <div class=" d-flex">
+                            <p class=" m-0 fs-2 me-3 text-nowrap">password :</p>
+                            <input type="text" name="name"
+                                class=" fs-4  form-control border-top-0 border-start-0 border-end-0 rounded-0 border-3 border-dark bg-transparent shadow-none"
+                                required>
+                        </div>
+                    </div>
+
+                </div>
+                <div class=" d-flex justify-content-end mt-3">
+                    <div class=" ms-auto">
+                        <button class=" btn btn-green-500 rounded-0 btn-lg px-5 ">Submit</button>
+                        <a class=" btn btn-red-500 rounded-0 btn-lg px-5">Back</a>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
